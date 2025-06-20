b28246656b909f89a8ac56c318d5a935b6302076
LuckylisaBemeye <166706078+LuckylisaBemeye@users.noreply.github.com>
2025-06-20 10:00:51 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,13 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):  # Outer loop
+        for j in range(n - i - 1):  # Inner loop
+            if arr[j] > arr[j + 1]:
+               temp = arr[j]
+               arr[j] = arr[j + 1]
+               arr[j + 1] = temp
+
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(numbers)
+print("Sorted array is:", numbers)
